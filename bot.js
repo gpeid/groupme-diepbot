132a04e7983c2013dcca9ce240b7f0a8b621095e
George Diep <ghdiep@gmail.com>
2025-11-01 14:22:32 -0400

    change to fetch call to post message

--- bot.js
+++ bot.js
@@ -8,52 +8,74 @@ const botRegex = /^\/cool guy|gabujeeno|kylan|tree of wisdom$/;
 
 function respond() {
   console.log(this.req.body);
-  const request = this.req.body;
-  const text = request.text.toLowerCase().trim();
-  if (request.text && botRegex.test(text)) {
-    postMessage();
-  } else {
-    console.log("don't care");
-  }
-}
-
-function postMessage() {
   const botResponse = cool();
 
-  const options = {
-    hostname: "api.groupme.com",
-    path: "/v3/bots/post",
-    method: "POST",
-  };
-  const body = {
-    bot_id: botID,
-    text: botResponse,
-  };
+  const request = this.req.body;
+  const text = request?.text?.toLowerCase().trim();
 
-  console.log("sending " + botResponse + " to " + botID);
+  const botResponseText =
+    request?.text && botRegex.test(text)
+      ? botResponse
+      : "No match response found for text";
 
-  const botReq = HTTPS.request(options, (res) => {
-    console.log(res.statusCode);
-
-    // console.log(util.inspect(res.statusCode, false, 1, true));
-
-    if (res.statusCode === 200) {
-      console.log(res.statusCode);
-    } else if (res.statusCode === 202) {
-      console.log("message posted successfully");
-    } else {
-      console.log(`rejecting bad status code ${res.statusCode}"`);
-    }
-  });
-
-  botReq.on("error", (err) => {
-    console.log(`error posting message ${JSON.stringify(err)}`);
-  });
-  botReq.on("timeout", (err) => {
-    console.log(err);
-    console.log(`timeout posting message ${JSON.stringify(err)}`);
-  });
-  botReq.end(JSON.stringify(body));
+  console.log(request.text, botRegex.test(text));
+  request?.text && botRegex.test(text)
+    ? postMessage(botResponseText)
+    : console.log("Text ignored don't care");
 }
 
+async function postMessage(text) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      bot_id: botID,
+      text: text,
+    }),
+  };
+  try {
+    console.log("Posting bot response...");
+
+    const response = await fetch(
+      `https://api.groupme.com/v3/bots/post/`,
+      options
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    console.log(response);
+
+    // const recentMessages = data.response.messages.map((msg) => msg.text);
+  } catch (error) {
+    console.error("Error fetching messages:", error);
+  }
+  console.log(`sending ${text} to ${botID}`);
+
+  // const botReq = HTTPS.request(options, (res) => {
+  //   console.log(res.statusCode);
+
+  //   // console.log(util.inspect(res.statusCode, false, 1, true));
+
+  //   if (res.statusCode === 200) {
+  //     console.log(res.statusCode);
+  //   } else if (res.statusCode === 202) {
+  //     console.log("message posted successfully");
+  //   } else {
+  //     console.log(`rejecting bad status code ${res.statusCode}"`);
+  //   }
+  // });
+
+  // botReq.on("error", (err) => {
+  //   console.log(`error posting message ${JSON.stringify(err)}`);
+  // });
+  // botReq.on("timeout", (err) => {
+  //   console.log(err);
+  //   console.log(`timeout posting message ${JSON.stringify(err)}`);
+  // });
+  // botReq.end(JSON.stringify(body));
+}
 exports.respond = respond;
