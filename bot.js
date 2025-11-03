6634e4917805e2af79edf00368ff447a24fff25f
George Diep <ghdiep@gmail.com>
2025-11-03 00:42:10 -0500

    test this fetch pattern

--- bot.js
+++ bot.js
@@ -3,50 +3,65 @@ const cool = require("cool-ascii-faces");
 const botID = process.env.BOT_ID;
 
 // const botRegex = /^\/cool guy|gabujeeno|kylan|tree of wisdom$/;
-// const botRegex = /\bgabujeeno\b|\bkylan\b|\btree of wisdom\b/i;
+const botRegex = /\bgabujeeno\b|\bkylan\b|\btree of wisdom\b/i;
 
 function respond() {
-  console.log(this.req.body);
   const botResponse = cool();
 
   const request = this.req.body;
+  console.log("groupme post body", request);
   const text = request?.text?.toLowerCase().trim();
 
-  const botResponseText = botResponse;
-
-  // console.log(request.text, botRegex.test(text));
-  // request?.text && botRegex.test(text)
-  request?.text && request?.text === "kylan"
-    ? postMessage(botResponseText)
+  console.log("regex test", botRegex.test(text));
+  request?.text && botRegex.test(text)
+    ? postMessage(botResponse)
     : console.log("Text ignored don't care");
 }
 
 async function postMessage(text) {
-  const options = {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const requestOptions = {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: myHeaders,
     body: JSON.stringify({
       bot_id: botID,
       text: text,
     }),
+    redirect: "follow",
   };
-  try {
-    console.log("Posting bot response...");
 
-    await fetch(`https://api.groupme.com/v3/bots/post`, options)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        console.log(response);
-      })
-      .catch((error) => {
-        console.error("Error during fetch:", error); // Handle any errors
-      });
-  } catch (error) {
-    console.error("Error fetching messages:", error);
-  }
+  fetch("https://api.groupme.com/v3/bots/post/", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
+  // const options = {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     bot_id: botID,
+  //     text: text,
+  //   }),
+  // };
+  // try {
+  //   console.log("Posting bot response...");
+
+  //   await fetch(`https://api.groupme.com/v3/bots/post`, options)
+  //     .then((response) => {
+  //       console.log(response);
+
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! status: ${response.status}`);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error during fetch:", error); // Handle any errors
+  //     });
+  // } catch (error) {
+  //   console.error("Error fetching messages:", error);
+  // }
 }
 exports.respond = respond;
