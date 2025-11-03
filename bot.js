d34f0a226bd988d178e6843e59f08cbb77935a2c
George Diep <ghdiep@gmail.com>
2025-11-03 17:02:28 -0500

    clean up

--- bot.js
+++ bot.js
@@ -1,73 +1,13 @@
 const axios = require("axios");
 const cool = require("cool-ascii-faces");
+const { buildRegexStringFromArray } = require("./src/utils/utils");
 
 const botID = process.env.BOT_ID;
 const baseUrl = process.env.GROUPME_BASE_URL;
 
-// const botRegex = /^\/cool guy|gabujeeno|kylan|tree of wisdom$/;
-const botRegex = /\bgabujeeno\b|\bkylan\b|\btree of wisdom\b/i;
+const triggerWordsArray = ["kylan", "gabujeeno", "tree of wisdom", "gorf"];
 
-// function respond() {
-//   const botResponse = cool();
-
-//   const request = this.req.body;
-//   console.log("groupme post body", request);
-//   const text = request?.text?.toLowerCase().trim();
-
-//   console.log("regex test", botRegex.test(text));
-//   request?.text && botRegex.test(text)
-//     ? postMessage(botResponse)
-//     : console.log("Text ignored don't care");
-// }
-
-// function postMessage(text) {
-// const myHeaders = new Headers();
-// myHeaders.append("Content-Type", "application/json");
-
-// const requestOptions = {
-//   method: "POST",
-//   headers: myHeaders,
-//   body: JSON.stringify({
-//     bot_id: botID,
-//     text: text,
-//   }),
-//   redirect: "follow",
-// };
-
-// fetch("https://api.groupme.com/v3/bots/post/", requestOptions)
-//   .then((response) => response.text())
-//   .then((result) => console.log(result))
-//   .catch((error) => console.error(error));
-
-// Fetch api
-// const options = {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   body: JSON.stringify({
-//     bot_id: botID,
-//     text: text,
-//   }),
-// };
-// try {
-//   console.log("Posting bot response...");
-
-//   await fetch(`https://api.groupme.com/v3/bots/post`, options)
-//     .then((response) => {
-//       console.log(response);
-
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//       }
-//     })
-//     .catch((error) => {
-//       console.error("Error during fetch:", error); // Handle any errors
-//     });
-// } catch (error) {
-//   console.error("Error fetching messages:", error);
-// }
-// }
+const botRegex = buildRegexStringFromArray(triggerWordsArray);
 
 async function postMessage(req, res) {
   const botResponse = cool();
