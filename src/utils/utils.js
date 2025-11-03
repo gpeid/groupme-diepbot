49a1af6858d2de3c10d057c0d15d4d573e0e2f3f
George Diep <ghdiep@gmail.com>
2025-11-03 17:02:03 -0500

    add regex builder helper function

---
+++ src/utils/utils.js
@@ -0,0 +1,24 @@
+// Helper function to construct regex from array of trigger words
+// Regex is used to test whether a trigger word is has been sent in chat
+
+const buildRegexStringFromArray = (arrayOfKeywords) => {
+  const reduceArrayIntoString = arrayOfKeywords.reduce(
+    (accumulator, currentValue, index) => {
+      if (index === 0) {
+        return `\\b${currentValue}\\b`;
+      } else {
+        return `${accumulator}|\\b${currentValue}\\b`;
+      }
+    },
+    ""
+  );
+
+  const regexFromReduecedArrayIntoString = new RegExp(
+    `${reduceArrayIntoString}`,
+    "i" // case insensitive flag
+  );
+
+  return regexFromReduecedArrayIntoString;
+};
+
+exports.buildRegexStringFromArray = buildRegexStringFromArray;
