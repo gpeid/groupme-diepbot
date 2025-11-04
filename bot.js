4f00950db2cb817bc6f82a464c4dd2518ec18b80
George Diep <ghdiep@gmail.com>
2025-11-04 18:26:10 -0500

    update bot with dimsum and commands features

--- bot.js
+++ bot.js
@@ -1,21 +1,22 @@
 const axios = require("axios");
 const cool = require("cool-ascii-faces");
 const { buildRegexStringFromArray } = require("./src/utils/utils");
+const {
+  triggerWordsArray,
+  foodWordsArray,
+  dimSumWordsArray,
+  dimSumDishArray,
+  commandsArray,
+} = require("./src/data/triggerWords");
 
 const botID = process.env.BOT_ID;
 const baseUrl = process.env.GROUPME_BASE_URL;
 
-const triggerWordsArray = ["kylan", "gabujeeno", "tree of wisdom", "gorf"];
-const foodWordsArray = ["pho", "bbh", "eggroll", "lumpia"];
-
 const triggerWordsObject = {
   general: triggerWordsArray,
   food: foodWordsArray,
-};
-
-const botResponseObject = {
-  general: cool(),
-  food: "Ohhh YUMMY!",
+  dimSum: dimSumWordsArray,
+  commands: commandsArray,
 };
 
 const getTriggerWordTypeFromObject = (word) => {
@@ -32,20 +33,45 @@ const getTriggerWordTypeFromObject = (word) => {
   }
 };
 
+const listAllTriggerWordsCommand = (commandString) => {
+  switch (commandString) {
+    case "/list dimsum":
+      return dimSumWordsArray.join(", ");
+    default:
+      console.log("No command entered");
+  }
+};
+
 async function postMessage(req, res) {
   const request = req.body;
   const text = request?.text;
   const wordType = getTriggerWordTypeFromObject(text);
+
+  // Dimsum response logic
+  // use regex builder here to avoid tolowercase or keep it idk
+  const dish = dimSumDishArray.find(
+    (item) => item.name.toLowerCase() === text.toLowerCase()
+  );
+  const botResponseObject = {
+    general: cool(),
+    food: "YUM!!",
+    dimSum: `${dish?.name}: ${dish?.description}`,
+    commands: `Available trigger words: ${listAllTriggerWordsCommand(text)}`,
+  };
+
   const botResponse = botResponseObject[wordType];
 
   // replace with regex?
   const isTriggerWord = (requestWord) => {
-    const botRegex =
-      getTriggerWordTypeFromObject(requestWord) &&
-      buildRegexStringFromArray(
+    if (getTriggerWordTypeFromObject(requestWord)) {
+      const botRegex = buildRegexStringFromArray(
         triggerWordsObject[getTriggerWordTypeFromObject(requestWord)]
       );
-    return botRegex.test(requestWord);
+
+      return botRegex.test(requestWord);
+    } else {
+      return null;
+    }
   };
 
   const postToGroupmeBot = async () => {
